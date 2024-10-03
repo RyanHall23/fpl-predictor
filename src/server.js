067945ae31e8997bdd48787a451b0476bbb7e3f0
ryanhall23 <rj23hall@protonmail.com>
2024-10-03 22:49:52 +0100

    v0.1 - Initial work on predicted points and basic lineup / subs

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+app.get('/api/bootstrap-static', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://fantasy.premierleague.com/api/bootstrap-static/'
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching data from FPL API:', error);
+    res.status(500).json({ error: 'Error fetching data from FPL API' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Proxy server running on http://localhost:${port}`);
+});
