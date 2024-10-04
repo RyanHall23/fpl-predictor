ee17522f26b01ef0d9fadb079210cf7e1c3885bb
ryanhall23 <rj23hall@gmail.com>
2024-10-04 21:51:42 +0100

    v0.132 - Progress towards fetching live data, team handling, and UI changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Typography, Box } from '@mui/material';
+import { Container, Typography, Box, Paper } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 import TeamFormation from './components/TeamFormation';
 import Toast from './components/Toast';
 import useTeamData from './hooks/useTeamData';
@@ -31,11 +32,17 @@ const App = () => {
             {calculateTotalPredictedPoints(mainTeamData).toFixed(2)}
           </Box>
         </Typography>
-        <TeamFormation
-          mainTeam={mainTeamData}
-          benchTeam={benchTeamData}
-          onPlayerClick={handlePlayerClick}
-        />
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item md={10}>
+            <Paper elevation={3} sx={{ padding: 1, marginTop: 1 }}>
+              <TeamFormation
+                mainTeam={mainTeamData}
+                benchTeam={benchTeamData}
+                onPlayerClick={handlePlayerClick}
+              />
+            </Paper>
+          </Grid>
+        </Grid>
       </Box>
       <Toast message={toastMessage} />
     </Container>
