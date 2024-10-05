0b44c7d0722092b4041e90c30eea75f3e0c8896e
ryanhall23 <rj23hall@gmail.com>
2024-10-05 16:48:31 +0100

    v0.136 - Eslint / Prettier config and formatting

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
     }
   }, [snackbarMessage]);
 
-  const handleSnackbarClose = (event, reason) => {
+  const handleSnackbarClose = () => {
     setSnackbarOpen(false);
   };
 
@@ -37,38 +37,46 @@ const App = () => {
   return (
     <Container>
       <Box
-        sx={{
+        sx={ {
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
-        }}
+        } }
       >
-        <Typography variant="h4" align="center" gutterBottom>
+        <Typography variant="h4"
+          align="center"
+          gutterBottom>
           FPL Predictor
         </Typography>
-        <Typography variant="body1" align="center" gutterBottom>
-          Total Predicted Points:{' '}
-          <Box component="span" sx={{ fontWeight: 'bold' }}>
-            {calculateTotalPredictedPoints(mainTeamData).toFixed(2)}
+        <Typography variant="body1"
+          align="center"
+          gutterBottom>
+          Total Predicted Points:{ " " }
+          <Box component="span"
+            sx={ { fontWeight: 'bold' } }>
+            { calculateTotalPredictedPoints(mainTeamData).toFixed(2) }
           </Box>
         </Typography>
-        <Grid container spacing={2} justifyContent="center">
-          <Grid md={10}>
-            <Paper elevation={3} sx={{ padding: 1, marginTop: 1 }}>
+        <Grid container
+          spacing={ 2 }
+          justifyContent="center">
+          <Grid md={ 10 }>
+            <Paper elevation={ 3 }
+              sx={ { padding: 1, marginTop: 1 } }>
               <TeamFormation
-                mainTeam={mainTeamData}
-                benchTeam={benchTeamData}
-                onPlayerClick={handlePlayerClick}
+                mainTeam={ mainTeamData }
+                benchTeam={ benchTeamData }
+                onPlayerClick={ handlePlayerClick }
               />
             </Paper>
           </Grid>
         </Grid>
       </Box>
       <Snackbar
-        open={snackbarOpen}
-        autoHideDuration={6000}
-        onClose={handleSnackbarClose}
-        message={snackbarMessage}
+        open={ snackbarOpen }
+        autoHideDuration={ 6000 }
+        onClose={ handleSnackbarClose }
+        message={ snackbarMessage }
       />
     </Container>
   );
--- src/components/TeamFormation/TeamFormation.js
+++ src/components/TeamFormation/TeamFormation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Paper, Box } from '@mui/material';
 import PlayerCard from '../PlayerCard/PlayerCard';
 import Grid from '@mui/material/Grid2';
@@ -7,23 +8,23 @@ import './styles.css';
 const TeamFormation = ({ mainTeam, benchTeam, onPlayerClick }) => {
   const goalkeepers = mainTeam
     ? Object.values(mainTeam)
-        .filter((player) => player.position === 1)
-        .sort((a, b) => b.predicted_points - a.predicted_points)
+      .filter((player) => player.position === 1)
+      .sort((a, b) => b.predicted_points - a.predicted_points)
     : [];
   const defenders = mainTeam
     ? Object.values(mainTeam)
-        .filter((player) => player.position === 2)
-        .sort((a, b) => b.predicted_points - a.predicted_points)
+      .filter((player) => player.position === 2)
+      .sort((a, b) => b.predicted_points - a.predicted_points)
     : [];
   const midfielders = mainTeam
     ? Object.values(mainTeam)
-        .filter((player) => player.position === 3)
-        .sort((a, b) => b.predicted_points - a.predicted_points)
+      .filter((player) => player.position === 3)
+      .sort((a, b) => b.predicted_points - a.predicted_points)
     : [];
   const forwards = mainTeam
     ? Object.values(mainTeam)
-        .filter((player) => player.position === 4)
-        .sort((a, b) => b.predicted_points - a.predicted_points)
+      .filter((player) => player.position === 4)
+      .sort((a, b) => b.predicted_points - a.predicted_points)
     : [];
 
   const benchTeamData = benchTeam ? Object.values(benchTeam) : [];
@@ -42,99 +43,104 @@ const TeamFormation = ({ mainTeam, benchTeam, onPlayerClick }) => {
   // Find the player with the highest points
   const captain = mainTeam
     ? Object.values(mainTeam).reduce(
-        (max, player) =>
-          parseFloat(player.predicted_points) > parseFloat(max.predicted_points)
-            ? player
-            : max,
-        Object.values(mainTeam)[0]
-      )
+      (max, player) =>
+        parseFloat(player.predicted_points) > parseFloat(max.predicted_points)
+          ? player
+          : max,
+      Object.values(mainTeam)[0],
+    )
     : null;
 
   return (
-    <Grid container spacing={2} justifyContent="center">
-      <Grid size={10}>
-        <Paper className="main-paper">
+    <Grid container spacing={ 2 } justifyContent='center'>
+      <Grid size={ 10 }>
+        <Paper className='main-paper'>
           <Box>
-            <Grid container spacing={2} justifyContent="center">
-              <Grid size={12}>
-                <Grid container spacing={2} justifyContent="center">
-                  {goalkeepers.map((player, index) => (
-                    <Grid size={12} key={player.name}>
+            <Grid container spacing={ 2 } justifyContent='center'>
+              <Grid size={ 12 }>
+                <Grid container spacing={ 2 } justifyContent='center'>
+                  { goalkeepers.map((player, index) => (
+                    <Grid size={ 12 } key={ player.name }>
                       <PlayerCard
-                        player={player}
-                        onClick={() => onPlayerClick(player, 'main')}
-                        index={index}
-                        isCaptain={player === captain}
+                        player={ player }
+                        onClick={ () => onPlayerClick(player, 'main') }
+                        index={ index }
+                        isCaptain={ player === captain }
                       />
                     </Grid>
-                  ))}
+                  )) }
                 </Grid>
               </Grid>
-              <Grid size={12}>
-                <Grid container spacing={2} justifyContent="center">
-                  {defenders.map((player, index) => (
-                    <Grid size={2.4} key={player.name}>
+              <Grid size={ 12 }>
+                <Grid container spacing={ 2 } justifyContent='center'>
+                  { defenders.map((player, index) => (
+                    <Grid size={ 2.4 } key={ player.name }>
                       <PlayerCard
-                        player={player}
-                        onClick={() => onPlayerClick(player, 'main')}
-                        index={index}
-                        isCaptain={player === captain}
+                        player={ player }
+                        onClick={ () => onPlayerClick(player, 'main') }
+                        index={ index }
+                        isCaptain={ player === captain }
                       />
                     </Grid>
-                  ))}
+                  )) }
                 </Grid>
               </Grid>
-              <Grid size={12}>
-                <Grid container spacing={2} justifyContent="center">
-                  {midfielders.map((player, index) => (
-                    <Grid size={2.4} key={player.name}>
+              <Grid size={ 12 }>
+                <Grid container spacing={ 2 } justifyContent='center'>
+                  { midfielders.map((player, index) => (
+                    <Grid size={ 2.4 } key={ player.name }>
                       <PlayerCard
-                        player={player}
-                        onClick={() => onPlayerClick(player, 'main')}
-                        index={index}
-                        isCaptain={player === captain}
+                        player={ player }
+                        onClick={ () => onPlayerClick(player, 'main') }
+                        index={ index }
+                        isCaptain={ player === captain }
                       />
                     </Grid>
-                  ))}
+                  )) }
                 </Grid>
               </Grid>
-              <Grid size={12}>
-                <Grid container spacing={2} justifyContent="center">
-                  {forwards.map((player, index) => (
-                    <Grid size={2.4} key={player.name}>
+              <Grid size={ 12 }>
+                <Grid container spacing={ 2 } justifyContent='center'>
+                  { forwards.map((player, index) => (
+                    <Grid size={ 2.4 } key={ player.name }>
                       <PlayerCard
-                        player={player}
-                        onClick={() => onPlayerClick(player, 'main')}
-                        index={index}
-                        isCaptain={player === captain}
+                        player={ player }
+                        onClick={ () => onPlayerClick(player, 'main') }
+                        index={ index }
+                        isCaptain={ player === captain }
                       />
                     </Grid>
-                  ))}
+                  )) }
                 </Grid>
               </Grid>
             </Grid>
           </Box>
         </Paper>
       </Grid>
-      <Grid size={10}>
-        <Paper className="bench-paper">
+      <Grid size={ 10 }>
+        <Paper className='bench-paper'>
           <Box>
-            <Grid container spacing={2} justifyContent="center">
-              {sortedBenchTeamData.map((player, index) => (
-                <Grid size={3} key={player.name}>
+            <Grid container spacing={ 2 } justifyContent='center'>
+              { sortedBenchTeamData.map((player, index) => (
+                <Grid size={ 3 } key={ player.name }>
                   <PlayerCard
-                    player={player}
-                    onClick={() => onPlayerClick(player, 'bench')}
-                    index={index}
+                    player={ player }
+                    onClick={ () => onPlayerClick(player, 'bench') }
+                    index={ index }
                   />
                 </Grid>
-              ))}
+              )) }
             </Grid>
           </Box>
         </Paper>
       </Grid>
     </Grid>
   );
+};
+TeamFormation.propTypes = {
+  mainTeam: PropTypes.object,
+  benchTeam: PropTypes.object,
+  onPlayerClick: PropTypes.func.isRequired,
 };
 
 export default TeamFormation;
