b23beb17bbeb77ac86bdd958e3203a1667be08e8
ryanhall23 <rj23hall@gmail.com>
2024-10-07 23:37:03 +0100

    v0.1392 - Add basic sidebar, dynamic user ID

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { Container, Typography, Box, Paper, Snackbar } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import TeamFormation from './components/TeamFormation/TeamFormation';
+import Sidebar from './components/Sidebar/Sidebar';
 import useTeamData from './hooks/useTeamData';
 
 const App = () => {
+  const [entryId, setEntryId] = useState('');
+  const [submittedEntryId, setSubmittedEntryId] = useState(null);
+
   const {
     mainTeamData,
     benchTeamData,
     snackbarMessage,
     handlePlayerClick,
     calculateTotalPredictedPoints,
-  } = useTeamData();
+  } = useTeamData(submittedEntryId);
 
   const [snackbarOpen, setSnackbarOpen] = useState(false);
 
@@ -34,8 +38,17 @@ const App = () => {
     }
   }, [snackbarOpen]);
 
+  const handleEntryIdSubmit = () => {
+    setSubmittedEntryId(entryId);
+  };
+
   return (
     <Container>
+      <Sidebar
+        entryId={ entryId }
+        setEntryId={ setEntryId }
+        handleEntryIdSubmit={ handleEntryIdSubmit }
+      />
       <Box
         sx={ {
           display: 'flex',
@@ -51,7 +64,7 @@ const App = () => {
         <Typography variant="body1"
           align="center"
           gutterBottom>
-          Total Predicted Points:{ " " }
+          Total Predicted Points:{ ' ' }
           <Box component="span"
             sx={ { fontWeight: 'bold' } }>
             { calculateTotalPredictedPoints(mainTeamData) }
