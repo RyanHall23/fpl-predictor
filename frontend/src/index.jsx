1db74d0415a4b1b663501127325527a0c96ad5c6
Ryan Hall <rj23hall@protonmail.com>
2026-07-06 21:59:49 +0100

    Revert "Install Vercel Speed Insights package (#299)" (#300)
    This reverts commit e83b1134fe2467416e82086ca2546bf797b58dab.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ThemeProvider } from './theme/ThemeContext'
-import { SpeedInsights } from '@vercel/speed-insights/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
-      <SpeedInsights />
     </ThemeProvider>
   </React.StrictMode>
 )
